6d97af7c082b7d3507a5f13cb7ea9058e8b42b37
Ktoxcon <kennethtoxro@gmail.com>
2020-09-15 16:59:19 -0600

    Role type changed from Role to string

--- src/database/interfaces/IUserModel.ts
+++ src/database/interfaces/IUserModel.ts
@@ -1,12 +1,9 @@
 import { Document } from "mongoose";
-import { UserRole } from "../util";
-
-type Role = UserRole.STUDENT | UserRole.TEACHER;
 
 export interface IUserModel extends Document {
   uid: string;
   name: string;
   lastname: string;
-  role: Role;
+  role: string;
   password: string;
 }
