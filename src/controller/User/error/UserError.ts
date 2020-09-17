d12f888abe735c56bbaa289f88cd16801a1a1f94
Ktoxcon <kennethtoxro@gmail.com>
2020-09-16 22:02:22 -0600

    New Errors added to UserError for handling Errors

--- src/controller/User/error/UserError.ts
+++ src/controller/User/error/UserError.ts
@@ -7,7 +7,12 @@ enum UserErrorCode {
   updateUser = 115,
   deleteUser = 120,
   getUsers = 125,
+  incorrectPassword = 130,
+  incorrectUsername = 135,
+  login = 140,
+  unauthorized = 145,
   hashingPassword = 150,
+  sessionExpired = 155,
 }
 
 const userAlreadyExistsError = new HttpError(
@@ -40,9 +45,34 @@ const userDoesntExistsError = new HttpError(
   "The user with that id doesn't exists."
 );
 
+const incorrectUsernameError = new HttpError(
+  UserErrorCode.incorrectUsername,
+  "Incorrect username"
+);
+
 const getUsersError = new HttpError(
   UserErrorCode.getUsers,
   "Error trying to get users."
+);
+
+const incorrectPasswordError = new HttpError(
+  UserErrorCode.incorrectPassword,
+  "Incorrect password"
+);
+
+const LoginError = new HttpError(
+  UserErrorCode.login,
+  "Login failed, try again later"
+);
+
+const unauthorizedError = new HttpError(
+  UserErrorCode.unauthorized,
+  "Unauthorized request"
+);
+
+const sessionExpiredError = new HttpError(
+  UserErrorCode.sessionExpired,
+  "Session expired"
 );
 
 export const UserError = {
@@ -52,5 +82,10 @@ export const UserError = {
   updateUserError,
   deleteUserError,
   getUsersError,
+  incorrectPasswordError,
+  incorrectUsernameError,
+  LoginError,
   passwordHashingError,
+  unauthorizedError,
+  sessionExpiredError,
 };
