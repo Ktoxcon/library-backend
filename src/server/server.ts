0ed357efb8bc7632aa0fd46dadf9f2904bcab28b
Ktoxcon <kennethtoxro@gmail.com>
2020-09-16 22:01:26 -0600

    final endpoint and routes added to the server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,15 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { adminRouter } from "../routes";
+import { userRouter } from "../routes";
 
 const server: Express = express();
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(cors());
+
+server.use("/library/v1", adminRouter, userRouter);
 
 export default server;
