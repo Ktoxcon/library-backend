38071a6989f953dd6726dd55c319d57fb45eb602
Ktoxcon <kennethtoxro@gmail.com>
2020-09-16 21:53:55 -0600

    Asymmetric encoding deleted

--- src/libraries/jsonwebtoken/JWT.ts
+++ src/libraries/jsonwebtoken/JWT.ts
@@ -1,25 +1,21 @@
 import * as jsonwebtoken from "jsonwebtoken";
 import { configuration } from "../../config";
 
-let { privateKey, publicKey, algorithm } = configuration.get("jwt") as any;
+let { privateKey, algorithm } = configuration.get("jwt") as any;
 
 export const sign = async (
   payload: any,
   options?: { expiresIn?: string | number }
 ) => {
-  return await jsonwebtoken.sign(
-    payload as any,
-    Buffer.from(privateKey, "base64"),
-    {
-      algorithm,
-      expiresIn: "1h",
-      ...options,
-    }
-  );
+  return await jsonwebtoken.sign(payload as any, privateKey, {
+    algorithm,
+    expiresIn: "1h",
+    ...options,
+  });
 };
 
 export const verify = async (token: string) => {
-  return await jsonwebtoken.verify(token, Buffer.from(publicKey, "base64"), {
+  return await jsonwebtoken.verify(token, privateKey, {
     algorithms: [algorithm],
   });
 };
