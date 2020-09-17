d194d28b2dfe3d7f5f1c5a8f97db2ef05caf52ce
Ktoxcon <kennethtoxro@gmail.com>
2020-09-16 22:03:06 -0600

    some methods added to UserController

--- src/controller/User/UserController.ts
+++ src/controller/User/UserController.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
 import { IUserModel, UserModel } from "../../database";
-import { generatePassword } from "../../libraries";
+import { generatePassword, sign, verifyPassword } from "../../libraries";
 import { UserError } from "./error/UserError";
+import { HttpError } from "../../libraries";
 
 const createUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    let { uid, name, lastname, role, password }: IUserModel = req.body.params;
+    let {
+      uid,
+      name,
+      lastname,
+      username,
+      role,
+      password,
+    }: IUserModel = req.body;
     let user: IUserModel = new UserModel();
 
     const userExists = await UserModel.findOne({
-      $and: [{ uid }, { name }, { lastname }],
+      $and: [{ uid }, { username }],
     });
 
     if (Boolean(userExists)) {
@@ -25,24 +33,41 @@ const createUser = async (req: Request, res: Response): Promise<void> => {
     user.uid = uid;
     user.name = name;
     user.lastname = lastname;
+    user.username = username;
     user.role = role;
     user.password = hashedPassword as string;
 
     const userSaved = await user.save();
 
-    if (Boolean(userSaved)) {
+    if (!Boolean(userSaved)) {
       res.status(500).send({ error: UserError.saveUserError });
     }
 
-    res.send(userSaved);
+    res.send({ userSaved, session: req.headers.authorization });
   } catch (err) {
-    res.send({});
+    res.send({ error: new HttpError(err.code, err.message) });
   }
 };
 
 const updateUser = async (req: Request, res: Response): Promise<void> => {
   try {
-  } catch (err) {}
+    const data = req.body;
+    const _id = req.params.id;
+
+    const userUpdated = await UserModel.findByIdAndUpdate(
+      _id,
+      { data },
+      { new: true }
+    );
+
+    if (!Boolean(userUpdated)) {
+      res.send({ error: UserError.updateUserError });
+    }
+
+    res.send(userUpdated);
+  } catch (err) {
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
 };
 
 const deleteUser = async (req: Request, res: Response): Promise<void> => {
@@ -60,7 +85,9 @@ const deleteUser = async (req: Request, res: Response): Promise<void> => {
     }
 
     res.send({ deleted: true });
-  } catch (err) {}
+  } catch (err) {
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
 };
 
 const getUsers = async (req: Request, res: Response): Promise<void> => {
@@ -72,11 +99,42 @@ const getUsers = async (req: Request, res: Response): Promise<void> => {
     }
 
     res.send(users);
-  } catch (err) {}
+  } catch (err) {
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
+};
+
+const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username });
+    if (!Boolean(user)) {
+      res.send({ error: UserError.incorrectUsernameError });
+    }
+
+    const isPasswordCorrect = await verifyPassword(password, user.password);
+
+    if (!Boolean(isPasswordCorrect)) {
+      res.send({ error: UserError.incorrectPasswordError });
+    }
+
+    const authToken = await sign(user.toJSON());
+
+    if (!Boolean(authToken)) {
+      res.send({ error: UserError.LoginError });
+    }
+
+    res.send({ authToken });
+  } catch (err) {
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
 };
 
 export const UserController = {
   createUser,
+  updateUser,
   deleteUser,
   getUsers,
+  login,
 };
