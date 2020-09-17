9129bb55e9086f881e3d3566100ebf8bdf57ac07
Ktoxcon <kennethtoxro@gmail.com>
2020-09-17 17:10:39 -0600

    InitBanner Method improved and displayError method added

--- src/libraries/logger/Logger.ts
+++ src/libraries/logger/Logger.ts
@@ -1,28 +1,70 @@
 import figlet from "figlet";
 
-export const initBanner = async () => {
+export const initBanner = (isAdminCreated: boolean) => {
   const colors = [
-    "\u001b[31m",
+    "\u001b[32m",
     "\u001b[32m",
     "\u001b[33m",
-    "\u001b[34m",
     "\u001b[36m",
     "\u001b[37m",
   ];
+  const separator = String("#").repeat(40);
+  const width = String(" ").repeat(2);
+  let currentColor = colors[Math.round(Math.random() * 5)];
+  let adminCreated = "";
 
-  let currentColor = colors[Math.round(Math.random() * 6)];
+  if (isAdminCreated) {
+    adminCreated = "DEFAULT ADMIN USER CREATED";
+  } else {
+    adminCreated = "DEFAULT ADMIN USER NOT CREATED";
+  }
 
-  await figlet.text("Library", (err, banner) => {
+  figlet.text(
+    "Library",
+    { font: "Doom", horizontalLayout: "universal smushing" },
+    (err, banner) => {
+      if (err) {
+        console.clear();
+        console.log("EXPRESS SERVER UP");
+        console.log("DB CONNECTION SUCCESS");
+        console.log(`${adminCreated}`);
+        return;
+      } else {
+        console.clear();
+        console.log(`${currentColor}${separator}`);
+        console.log(`${currentColor}${banner}`);
+        console.log(`${currentColor}${separator}`);
+        console.log(`${colors[2]}${width}[✓] SERVER UP`);
+        console.log(`${colors[2]}${width}[✓] DB CONNECTION SUCCES`);
+        console.log(`${colors[2]}${width}[✓] ${adminCreated}`);
+        console.log(`${currentColor}${separator}`);
+      }
+    }
+  );
+};
+
+export const displayError = (
+  name: string,
+  message?: string,
+  code?: string | number
+) => {
+  const separator = String("#").repeat(40);
+  const yellow = "\u001b[33m";
+  const red = "\u001b[31m";
+
+  figlet.text("Error", { font: "Doom" }, (err, Error) => {
     if (err) {
       console.clear();
-      console.log("EXPRESS SERVER UP");
-      console.log("DB CONNECTION SUCCESS");
+      console.log(`[*] ERROR NAME: ${name}`);
       return;
     } else {
-      console.clear();
-      console.log(`${currentColor}${banner}`);
-      console.log("[✓] SERVER UP");
-      console.log("[✓] DB CONNECTION SUCCES");
+      console.log(`${red}${separator}`);
+      console.log(`${yellow}${Error}`);
+      console.log(`${red}${separator}`);
+      console.log(`${yellow}[*] ERROR NAME: ${name}`);
+      console.log(`${yellow}[*] ERROR MESSAGE: ${message}`);
+      console.log(`${yellow}[*] ERROR CODE: ${code}`);
+      console.log(`${red}${separator}`);
     }
   });
 };
