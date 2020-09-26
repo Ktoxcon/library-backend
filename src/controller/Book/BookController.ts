ec4343cfa97e817be281c636485002e4e66d64b9
Ktoxcon <kennethtoxro@gmail.com>
2020-09-25 18:20:24 -0600

    BookController updated

--- src/controller/Book/BookController.ts
+++ src/controller/Book/BookController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { displayError, HttpError } from "libraries";
 import { DocumentQuery } from "mongoose";
 import { IBookModel, BookModel } from "../../database";
 import { BookError } from "./error/BookError";
@@ -13,8 +14,6 @@ const addBook = async (req: Request, res: Response): Promise<void> => {
       description,
       themes,
       copies,
-      disponibility,
-      loans,
     }: IBookModel = req.body;
 
     const book = new BookModel();
@@ -24,34 +23,72 @@ const addBook = async (req: Request, res: Response): Promise<void> => {
     });
 
     if (Boolean(bookExists)) {
-      const newCopyAdded = await addCopy(bookExists._id);
+      const newCopyAdded = await addCopy(bookExists._id, copies);
       if (!Boolean(newCopyAdded)) {
         res.status(500).send({ error: BookError.addCopyError });
+        return;
       } else {
         res.send(newCopyAdded);
       }
+    } else {
+      book.author = author;
+      book.title = title;
+      book.edition = edition;
+      book.keyWords = keyWords;
+      book.description = description;
+      book.themes = themes;
+      book.copies = copies;
+
+      const bookAdded = await book.save();
+
+      if (!Boolean(bookAdded)) {
+        res.status(500).send({ error: BookError.saveBookError });
+        return;
+      } else {
+        res.send(bookAdded);
+      }
     }
-
-    book.author = author;
-    book.title = title;
-    book.edition = edition;
-    book.keyWords = keyWords;
-    book.description = description;
-    book.themes = themes;
-    book.copies = copies;
-    book.disponibility = ;
-
-
-  } catch (err) {}
+  } catch (err) {
+    displayError(err.name, err.message, err.code);
+    res.status(500).send({ error: new HttpError(err.code, err.message) });
+  }
 };
 
-const addCopy = async (_id: any): Promise<IBookModel> => {
+const deleteBook = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const bookDeleted = await BookModel.findByIdAndDelete(req.params.id);
+    if (!Boolean(bookDeleted)) {
+      res.status(500).send({ error: BookError.deleteBookError });
+      return;
+    } else {
+      res.send({ deleted: true });
+    }
+  } catch (err) {
+    displayError(err.name, err.message, err.code);
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
+};
+
+const updateBook = async (req: Request, res: Response): Promise<void> => {
+  try {
+  } catch (err) {
+    displayError(err.name, err.message, err.code);
+    res.send({ error: new HttpError(err.code, err.message) });
+  }
+};
+
+const addCopy = async (_id: any, add: number): Promise<IBookModel> => {
   const copyToAdd = await BookModel.findByIdAndUpdate(
     _id,
     {
-      $inc: { copies: 1, disponibility: 1 },
+      $inc: { copies: add, disponibility: add },
     },
     { new: true }
   );
   return copyToAdd;
+};
+
+export const BookController = {
+  addBook,
+  deleteBook,
 };
